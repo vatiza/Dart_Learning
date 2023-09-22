580fd2cdd016396228cbedf38c868fefc57983e4
vatiza <rakibur.mac@gmail.com>
2023-09-22 22:00:47 +0600

    num

--- boolean.dart
+++ boolean.dart
@@ -3,6 +3,13 @@ void main() {
   greater = 2 > 3;
   print(greater);
   print(greater.runtimeType);
-  var num = "REaki";
-  print(num.runtimeType);
+  var num1 = "REaki";
+  print(num1.runtimeType);
+
+// num both int duble
+  num number = 3;
+  // print(number);
+
+  number = 3.2;
+  print(number);
 }
