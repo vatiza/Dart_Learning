b17b330406a6a0bdd3195c332700f6f190a5ea7e
vatiza <rakibur.mac@gmail.com>
2023-09-22 21:43:22 +0600

    syntax

---
+++ Syntax.dart
@@ -0,0 +1,13 @@
+void main() {
+  String country = "Japan";
+  print("i visit $country");
+
+  String s1 = 'String ' 'concatenation' " works even over line breaks.";
+  // print("The sum of 5 and 3 equals ${5 + 3}.");
+  print(s1);
+  String s1 = 'String '
+      "concatenation"
+      " works even over line breaks.";
+
+  print(s1);
+}
