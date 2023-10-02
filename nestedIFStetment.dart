9e6bed9f85145cccfb6d28525578f8af85d28f22
vatiza <rakibur.mac@gmail.com>
2023-10-02 22:05:50 +0600

    nestedIfStetment

---
+++ nestedIFStetment.dart
@@ -0,0 +1,18 @@
+void main(){
+  bool isWeekend=true;
+  String weather='rainday';
+  if(isWeekend){
+    if(weather=='sunny'){
+      print('Lets to go to inside home');
+    }
+    else{
+      print('no find sun');
+    }
+    if(weather=='rainday'){
+      print('if you going to outside? Please carrying umbrella! ');
+    }
+    else{
+      print('today is sunny');
+    }
+  }
+}
