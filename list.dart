cffc83050f1a418083ebc9d394b6ac248431eed2
vatiza <rakibur.mac@gmail.com>
2023-10-02 23:01:15 +0600

    list

---
+++ list.dart
@@ -0,0 +1,10 @@
+void main() {
+  List numbers = [1, 2, 3, 45, 67, 87];
+  print(numbers);
+  List fruits = ["apple", "banana", "cherry", "durian", "elderberry"];
+  print(fruits);
+  fruits.add('mula');
+  print(fruits[1]);
+  fruits.remove('apple');
+  print(fruits);
+}
--- switch.dart
+++ switch.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 void main(){
   print('Enter your id:');
-
+// https://vscode.dev/github/vatiza/Dart_Learning/blob/main/switch.dart#L6
+// https://github.com/vatiza/Dart_Learning/blob/864c1fae1c9a2e331fa648454e40e3dddd47a5b3/switch.dart#L4fdgfthrftr
   int id= int.parse(stdin.readLineSync()!);
   switch(id){
     case 82:
