c4ebc382290a93de2174e96872938dae52a21071
vatiza <rakibur.mac@gmail.com>
2023-10-03 22:51:24 +0600

    switchCase useMenu

---
+++ switchWithMenuCHoose.dart
@@ -0,0 +1,30 @@
+/*
+1. Coffee
+2. Tea
+3. Hot Chocolate
+4. Iced Tea
+5. Lemonade
+*/
+main(){
+  int choose=3;
+  switch(choose){
+    case 1:
+    print('You have chosen Coffee. Enjoy your drink!');
+    break;
+    case 2:
+    print('You have chosen Tea. Enjoy your drink!');
+    break;
+    case 3:
+    print('You have chosen Hot Chocolate. Enjoy your drink!');
+    break;
+    case 4:
+    print('You have chosen Iced Tea. Enjoy your drink!');
+    break;
+    case 5:
+    print('You have chosen Lemonade. Enjoy your drink!');
+    break;
+
+
+  }
+  
+}
